985227cfbee739c31463fed2e03e9e4f7a90b3f8
arzaluz-chris <23008381@es.uveg.edu.mx>
2025-04-02 11:09:01 -0600

    Initial Commit

---
+++ BishopTest/BishopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BishopTestApp.swift
+//  BishopTest
+//
+//  Created by Christian Arzaluz on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BishopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BishopTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BishopTest
+//
+//  Created by Christian Arzaluz on 02/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
